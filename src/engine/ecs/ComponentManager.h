a1a43870d02af3095e8f1e1636efb4d72643b3cd
Alex Khouderchah <alex.khouderchah@gmail.com>
2019-07-05 11:46:32 -0700

    build_tool: Generate explicit ComponentManager instantiations
    Previously, ComponentManager.h contained the ComponentManager method
    definitions. This change moves those definitions to a *.inc file,
    which is not included by any normal file. build_tool then generates a
    ComponentManager.cpp file which includes the .inc file and explicitly
    instantiates ComponentManager for every component type defined in the
    project.
    
    Doing this centralizes all ComponentManager instantiations into a
    single object file. Since the instantiations are not placed in a
    header file, adding a new component type will not require all files
    that include ComponentManager.h to recompile. Also, while
    ComponentManager.cpp needs to include every header that defines a new
    component type, this does not spill over to ComponentManager.h,
    preventing a scenario where adding a new component type will require
    most of the project to recompile.

--- src/engine/ecs/ComponentManager.h
+++ src/engine/ecs/ComponentManager.h
@@ -7,7 +7,6 @@
 #include "core/ErrorSystem.h"
 #include "core/Guid.h"
 #include "core/ObjectHandle.h"
-#include "engine/ecs/Entity.h"
 #include "engine/ecs/IComponentManager.h"
 
 namespace tetrad {
@@ -42,95 +41,6 @@ private:
 	std::vector<T*> m_pComponents;
 	static ObjectHandle::type_t s_ID;
 };
-
-template <typename T>
-ComponentManager<T>::ComponentManager()
-{
-#ifdef _DEBUG
-	std::cout << "Creating manager of type: " << typeid(T).name() << std::endl;
-#endif  // _DEBUG
-
-	// Add the null element
-	m_pComponents.push_back(new T(kNullEntity));
-
-	// @TODO Don't construct null element?
-	//T *pComponent = (T*)::operator new (sizeof(T));
-	//m_pComponents.push_back(pComponent);
-}
-
-template <typename T>
-ComponentManager<T>::~ComponentManager()
-{
-	//::operator delete(&m_pComponents[0]);
-
-	for(size_t i = 0; i < m_pComponents.size(); ++i)
-	{
-		delete m_pComponents[i];
-	}
-	m_pComponents.clear();
-}
-
-template <typename T>
-ObjectHandle::ID_t ComponentManager<T>::Add(IComponent *pComponent)
-{
-	size_t i = m_pComponents.size();
-	if(ObjectHandle::MAX_ID <= i)
-	{
-		LOG("Failed to allocate space for a new element! Perhaps remove some existing entities/components or expand the ObjectHandle sizes.\n");
-		return 0;
-	}
-
-	m_pComponents.push_back((T*)pComponent);
-	return (ObjectHandle::ID_t)i;
-}
-
-template <typename T>
-ObjectHandle::ID_t ComponentManager<T>::Delete(ObjectHandle::ID_t index)
-{
-	size_t size = m_pComponents.size();
-	if(index >= size || index == 0){ return 0; }
-
-	delete m_pComponents[index];
-	m_pComponents[index] = m_pComponents.back();
-	m_pComponents.pop_back();
-
-	if(index == size-1){ return 0; }
-	return index;
-}
-
-template <typename T>
-void ComponentManager<T>::DeleteAll()
-{
-	// Delete and pop back everything but the 0 element
-	for(size_t i = m_pComponents.size(); i-->1;)
-	{
-		delete m_pComponents[i];
-		m_pComponents.pop_back();
-	}
-}
-
-template <typename T>
-IComponent *ComponentManager<T>::Get(ObjectHandle::ID_t index) const
-{
-	if(index >= m_pComponents.size() || index == 0)
-	{
-		index = 0;
-	}
-
-	return m_pComponents[index];
-}
-
-template <typename T>
-ConstVector<T*> ComponentManager<T>::GetAll()
-{
-	return ConstVector<T*>(m_pComponents);
-}
-
-template <typename T>
-IComponent *ComponentManager<T>::operator[](ObjectHandle::ID_t index) const
-{
-	return this->Get(index);
-}
 
 /**
  * @brief Full specialization of the GUID class so as to allow for the compile-time adding of
